a79fcf6eb50d1f04556e33e18b44555e0b6f2418
mari.17 <fujihira.marina2010@gmail.com>
2024-12-23 23:57:42 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+      
+      return(
+      <div>
+      <h1>SEVENTEEN</h1>
+      <button>ジョンハン</button>
+      <button>ジュン</button>
+      
+      </div>
+      );
+    }
+}
+export default App;
